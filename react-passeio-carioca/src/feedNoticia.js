ac6a30b340d6bf03bd519e039d2de02e319f2541
Guilherme <guibma@hotmail.com>
2023-11-12 19:51:20 -0300

    alteração de nome de arquivos png

--- react-passeio-carioca/src/feedNoticia.js
+++ react-passeio-carioca/src/feedNoticia.js
@@ -4,48 +4,48 @@ const newsFeedData = [
   {
     id: 1,
     title: "Edifício Natal",
-    imagePath: "./imagens/Praça Floriano, n°45.png",
+    imagePath: "./imagens/EdifícioNatal.png",
     content: "Com vocês o Edifício Natal, um dos prédios em estilo art déco mais belos do Brasil. Após a demolição do lendário Convento da Ajuda, no início do século passado, começaram a ser erguidas construções com inspiração francesa. Algumas delas ganharam cinemas em suas lojas no térreo. Assim surgiu a Cinelândia.O Edifício Natal, projetado por Ricardo Wriedt, ficou pronto em 1927, e logo em seguida foi inaugurado o Cine Pathé, com uma sala de dois pavimentos e quase mil lugares. A entrada para o cinema era através da Praça Floriano, n°45, e a do Edifício Natal fica nos fundos do prédio, na Rua Álvaro Alvim, n° 48. O cinema, de propriedade da família do fotógrafo Marc Ferrez, funcionou no local por 70 anos, até ser fechado em 1998. O prédio em seguida foi restaurado mantendo as características originais, como o piso dos andares, ferragens e mármores das escadas, janelas e principalmente, a fachada com um dos coroamentos mais lindos da cidade."
   },
   {
     id: 2,
     title: "Edifício Estrela",
-    imagePath: "./imagens/Visconde de Pirajá, nº 336.png",
+    imagePath: "./imagens/EdifícioEstrela.png",
     content: "Em 1953 foi inaugurado na Rua Visconde de Pirajá, nº 336, em Ipanema o Edifício Estrela. Construído ao lado da Praça Nossa Senhora da Paz entre o mais badalado ponto da praia de Ipanema e a Lagoa Rodrigo de Freitas e o Cristo Redentor (que aparece nessa foto), o Estrela é um dos prédios mais tradicionais e antigos do bairro.Seu primeiro bloco possui 10 apartamentos mais a cobertura, distribuídos em cinco andares.As unidades do térreo são de quarto e sala enquanto as demais apresentam dois e três dormitórios. Atravessando um corredor extenso o morador encontra um pequeno jardim que divide as construções. Caminhando mais alguns metros está o segundo bloco, que é mais simples e menor que o virado para a Rua Visconde de Pirajá. São mais cinco apartamentos distribuídos pelos três andares do edifício. Ao todo ele conta com 16 unidades e as duas lojas do térreo. O coroamento escalonado, suas varandas arredondadas, a porta de entrada em madeira e os detalhes internos em mármore fazem do Estrela um prédio admirável. Já reparou nessa Estrela arquitetônica de mais de 70 anos?"
   },
   {
     id: 3,
     title: "Edifício Alagoas",
-    imagePath: "./imagens/Rua Ministro Viveiros de Castro,122.png",
+    imagePath: "./imagens/EdifícioAlagoas.png",
     content: "Na Rua Ministro Viveiros de Castro,122, em Copacabana, foi inaugurado em 1933 o Edifício Alagoas, um exemplar extraordinário do estilo art déco. Antes de visitar o interior do Alagoas encontrei gravado no granito escuro da fachada informações que indicam que o prédio foi projetado por Angelo Bruhns e construído por Penna & França. São seis andares e vinte e quatro apartamentos de dois e três quartos ao todo. A fachada do Alagoas tem formato de proa de navio,  evidenciando que o prédio faz parte da vertente náutica do estilo art déco. O portão em ferro e sem vidros de 90 anos com detalhes em dourado é um de seus pontos fortes. O hall de entrada é um dos mais charmosos de Copacabana. Três tipos de mármores com cores diferentes decoram a portaria repleta de volumetria.  São dois elevadores de portas pantográficas, além de uma escada iluminada naturalmente graças a uma parede em madeira e vidro.  O piso em frente as portas principais dos apartamentos é de pastilhas brancas e verdes e a numeração das unidades vai de 1 a 24, ou seja,  no último andar estão os apartamentos 21, 22, 23 e 24. O térreo conta com duas lojas comerciais fechadas há muitos anos.  Elas são usadas pelo vizinho Hotel Royal Rio Palace. No início da década de 1990 o hotel comprou as lojas e  começou a adquirir alguns apartamentos com a intenção de se tornar o único dono do prédio. Provavelmente para realizar a sua demolição e construir em seu terreno uma expansão do hotel. Graças ao tombamento realizado pela Prefeitura do Rio essa ideia não foi a frente."
   },
   {
     id: 4,
     title: "Fonseca e Silva",
-    imagePath: "./imagens/Rua Alberto de Campos, nº 217.png",
+    imagePath: "./imagens/FonsecaeSilva.png",
     content: "O Edifício que inspirou a série Detetives do Prédio Azul, do canal Gloob, também foi o lar de Ana Maria Teixeira de Carvalho, a eterna musa da bossa-nova e Mulher de Branco de Ipanema, que faleceu no último dia 3 de agosto, aos 75 anos. Há décadas ela circulava pelas ruas de Ipanema como a mais famosa e polêmica personalidade do bairro.Era filha do radialista e jornalista Luis de Carvalho e ex-mulher do compositor Marcos Valle, com quem ajudou a divulgar a bossa-nova pelo mundo.Ana Maria, há 16 anos, vendeu sua casa branca na Rua Joana Angélica, nº 207 e comprou um apartamento no térreo do prédio azul, onde vivia com uma acompanhante desde 2007.O nome verdadeiro do prédio azul é Fonseca e Silva, localizado na Rua Alberto de Campos, nº 217, em Ipanema. Construído em 1938,ele conta com quatro apartamentos e duas coberturas, todos com 3 quartos e cerca de 100 m2.Foi erguido pelo Conde de Modesto Leal (1860-1939), considerado o homem mais rico da República Velha e um dos primeiros a habitar Ipanema.Sua fachada art déco é usada desde 2012 para a serie infantil “Detetives do Prédio Azul”, exibida pelo canal Gloob e pela TV Brasil e que há poucos anos até virou filme.O nome da serie poderia ser outro já que a construção no passado foi cinza, rosa e originalmente era revestida de pó de pedra."
   },
   {
     id: 5,
     title: "Les Palais Botafogo",
-    imagePath: "./imagens/Rua Assunção, n° 2.png",
+    imagePath: "./imagens/LesPalaisBotafogo.png",
     content: "Na Rua Assunção, n° 2, em Botafogo encontrei o Condomínio Les Palais Botafogo, um oásis residencial repleto de histórias e com duas joias arquitetônicas. Atrás de um lindo portão antigo restaurado estão dois imóveis centenários tombados e nove edifícios,inaugurados em 2011, dividindo o mesmo terreno em completa harmonia visual.Um desses imóveis antigos é o Chalet Olinda erguido no início do século XIX, quando Botafogo ainda conservava o seu aspecto de chácara.Pedro de Araújo Lima, o Marquês de Olinda vendeu essa casa em 1855 ao Dr. Antônio Peixoto. O novo dono adaptou as amplas instalações do Chalet Olindapara atendimento hospitalar. Nesse local fundou a Casa de Saúde Dr. Peixoto com quartos com vista para o mar. Em 1865, o Chalet foi comprado peloDr. Manoel Fernandes Eiras e a clínica mudou de nome. A partir de 1951, a Casa de Saúde Dr. Eiras passou a abrigar a Clínica de Neurocirurgia de Paulo Niemeyer.Com o passar dos anos a clínica fechou e o terreno de quase 35 mil metros quadrados foi comprado para a construção do Les Palais Botafogo, um condomínioque mistura dois imóveis protegidos e tombados por legislação municipal (Chalet Olinda e Pavilhão Santa Clarice) com nove edifícios residenciais.As duas construções antigas foram totalmente restauradas e hoje abrigam os diversos equipamentosde lazer do condomínio (salão de artes, piscinas, salão de beleza, sala de leitura, academia...), que conta com 120 apartamentosde 3 ou 4 quartos com 171 m2 a 226 m2. A construção dos prédios teve início em 2007 e finalizada em 2011. Toda a vegetação do local foi mantida.O Chalet Olinda possui estilo romântico evocando a bucólica vida no campo enquanto o Pavilhão Santa Clarice (com a fachada amarela)é um prédio neoclássico com elementos ecléticos como o símbolo da medicina em seu frontão."
   },
   {
     id: 6,
     title: "Edifício Urca",
-    imagePath: "./imagens/Avenida Portugal, n° 986.png",
+    imagePath: "./imagens/EdifícioUrca.png",
     content: "Entre a Praia da Urca e o Morro do Pão de Açúcar na Avenida Portugal, n° 986, foi inaugurado em 1934 o Edifício Urca, praticamenteo primeiro grande prédio do bairro no estilo art déco. Essa construção guarda alguns tesouros arquitetônicos.A começar com o nome do edifício eternizado com letras típicas do estilo art déco e o portão de ferro com algunselementos marinhos (peixes, cavalos-marinhos, bolhas de ar, algas, caracóis) que aumentam a impressão do Edifício Urcaser um navio ancorado no porto prestes a zarpar. Isso graças ao seu formato e a proximidade com a areia e o espelho d'água.Emoldurando a portada de granito preto estão duas colunas encimadas por luminárias embutidas. Um clássico dos prédios chiques dessa época.O hall de entrada do edifício é típico da década de 1930, com mármores no piso e sobrepostos em parte das paredes.Uma passadeira vermelha indica o caminho aos elevadores de portas pantográficas, que contam com grandes maçanetase elementos de respiração/visualização em formato de flor.O Urca foi projetado e construído pelo engenheiro Alfredo Baumann, tendo como primeiro proprietário Antônio Belmiro Rodrigues.Alfredo Baumann estudou na Europa por dois anos, onde se especializou em construções de cimento armado. Ele é filho de Charles Baumann, diretor do extinto Banco Brasileiro-Alemão."
   },
   {
     id: 7,
     title: "Casa França-Brasil",
-    imagePath: "./imagens/Rua Visconde de Itaboraí, n° 78.png",
+    imagePath: "./imagens/CasaFrança-Brasil.png",
     content: "A Casa França-Brasil foi a primeira construção neoclássica do Rio de Janeiro. Inaugurada em 13 de maio de 1820, na Rua Visconde de Itaboraí, n° 78, é um marcoda arquitetura nacional.Foi projetada por Grandjean de Montigny, o mesmo que projetou o Solar da PUC-Rio, integrante da Missão Artística Francesa e professor da Academia Imperial de Belas Artes. O prédio foi construído para ser a primeira praça do Comércio no Rio de Janeiro (Bolsa de Valores da época). Seu interior foi inspirado nas basílica cívicas romanas. Repleto de abóbodas o espaço é cercado de pórticos dóricos.Foi tombado em 1938 pelo Instituto do Patrimônio Histórico e Artístico Nacional. Até 1944 foi Alfândega. De 1942 até 1952, foi depósito dos bancos Ítalos-Germânicos e de 1956 até 1978, foi o II Tribunal do Júri.Até o dia 16 de abril de 2023 a Casa França-Brasil recebe a exposição Lugar Geométrico de Raul Mourão. As enormes obras de arte em constante movimento em seu interior são dessa exposição. Uma dessas esculturas decora a parte externa da galeria de arte Lurixs, na Rua Dias Ferreira, n° 214, no Leblon."
   }
 ];
 
-const NewsFeedItem2 = ({ item }) => {
+const NewsFeedItem = ({ item }) => {
   return (
     <div style={{ border: '1px solid #ccc', margin: '10px', padding: '10px' }}>
       <button onClick={() => console.log('Back clicked')}>Voltar</button>
@@ -66,36 +66,10 @@ const NewsFeed = () => {
   return (
     <div>
       {newsFeedData.map(item => (
-        <NewsFeedItem2 key={item.id} item={item} />
+        <NewsFeedItem key={item.id} item={item} />
       ))}
     </div>
   );
 };
-
-
-// const NewsFeedItem = () => {
-
-//   return (
-//     <div style={{ border: '1px solid #ccc', margin: '10px', padding: '10px' }}>
-//       <button onClick={() => console.log('Back clicked')}>Voltar</button>
-//       <h2>Edifício Natal</h2>
-//       <img src={require("./imagens/Visconde de Pirajá, nº 336.png")} alt="Edifício Natal" style={{width: '25em', height: '25em'}} />
-//       <p>Com vocês o Edifício Natal, um dos prédios em estilo art déco mais belos do Brasil. Após a demolição do lendário Convento da Ajuda, no início do século passado, começaram a ser erguidas construções com inspiração francesa. Algumas delas ganharam cinemas em suas lojas no térreo. Assim surgiu a Cinelândia.O Edifício Natal, projetado por Ricardo Wriedt, ficou pronto em 1927, e logo em seguida foi inaugurado o Cine Pathé, com uma sala de dois pavimentos e quase mil lugares.A entrada para o cinema era através da Praça Floriano, n°45, e a do Edifício Natal fica nos fundos do prédio, na Rua Álvaro Alvim, n° 48.O cinema, de propriedade da família do fotógrafo Marc Ferrez, funcionou no local por 70 anos, até ser fechado em 1998.O prédio em seguida foi restaurado mantendo as características originais, como o piso dos andares, ferragens e mármores das escadas, janelase principalmente, a fachada com um dos coroamentos mais lindos da cidade.</p>
-//       <button onClick={() => console.log('Mapa')}>Botão para o mapa</button>
-//       <div>
-//         <button onClick={() => console.log('Curtir')}>Botão de curtir</button>
-//         <button onClick={() => console.log('Compartilhar')}>Botão de compartilhar</button>
-//         <button onClick={() => console.log('Comentar')}>Botão de comentar</button>
-//       </div>
-//     </div>
-//   );
-// };
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <NewsFeedItem />
-//     {/* Outros componentes aqui */}
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
 export default NewsFeed;
