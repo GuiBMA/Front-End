94e268ad5ac5dc211ca9adbbce87ba3df57d28ca
Guilherme <guibma@hotmail.com>
2023-11-15 09:30:50 -0300

    Atualização do feed

--- react-passeio-carioca/src/index.js
+++ react-passeio-carioca/src/index.js
@@ -8,7 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <NewsFeed/>
-    <App />
-    {/* <NewsFeedItem/> */}
+    {/* <App/> */}
   </React.StrictMode>
 );
